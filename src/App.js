8a4b6dbfb93156e907f6dea8d5062604e6274e61
zsdtlucky <14130477+zsdtlucky@user.noreply.gitee.com>
2025-12-06 15:52:18 +0800

    react构成的quiz前端

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Admin from './Admin';
+
+const App = () => {
+  return <Admin />;
+};
+
+export default App;
+
+
+
+// import React, { useState ,useEffect } from 'react';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import Login from './Login';
+// import Admin from './Admin';
+
+// const App = () => {
+//    
+//   const [isLogin, setIsLogin] = useState(() => localStorage.getItem('isLogin') === 'true');
+
+//     useEffect(() => {
+//    
+//     if (isLogin) {
+//       localStorage.setItem('isLogin', 'true');
+//     } else {
+//       localStorage.removeItem('isLogin');
+//     }
+//   }, [isLogin]);
+
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route
+//           path="/login"
+//           element={<Login onLogin={() => setIsLogin(true)} />}
+//         />
+//         <Route
+//           path="/admin"
+//           element={
+//             isLogin ? <Admin /> : <Navigate to="/login" replace />
+//           }
+//         />
+//         <Route
+//           path="/"
+//           element={<Navigate to="/login" replace />}
+//         />
+//       </Routes>
+//     </Router>
+//   );
+// };
+// export default App;
